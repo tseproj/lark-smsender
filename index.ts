43127a191f09db234d9d2d3f61594b9a5cc48d55
李水光 <liwtlt@cladonia.team>
2024-08-12 13:22:55 +0800

    feat: serve form ui

--- index.ts
+++ index.ts
@@ -1,3 +1,22 @@
-const foo = 'bar'
+import { serve } from '@hono/node-server'
+import { serveStatic } from '@hono/node-server/serve-static'
+import { Hono } from 'hono'
 
-console.log(foo)
+const app = new Hono()
+
+app.use('/', serveStatic({ root: './static/' }))
+app.get(
+  '/assets/*',
+  serveStatic({
+    root: './static/assets',
+    rewriteRequestPath: (path) =>
+      path.replace(/^\/assets/, ''),
+  })
+)
+const port = 3000
+console.log(`server running at ${port}`)
+
+serve({
+  fetch: app.fetch,
+  port: port
+})
